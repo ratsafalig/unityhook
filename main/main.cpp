ef4e291e9d946db73551d9276ae5b5e7979d9e17
ratsafalig <ratsafalig@outlook.com>
2025-03-31 00:03:35 +0800

    upd: main project update

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,13 @@
 ﻿#include <iostream>
 
+#include "Windows.h"
+#include "windows.h"
+
+#include <BlackBone/Process/Process.h>
+#include <BlackBone/Patterns/PatternSearch.h>
+#include <BlackBone/Process/RPC/RemoteFunction.hpp>
+#include <BlackBone/Syscalls/Syscall.h>
+
 #include "MinHook.h"
 
 int main()
