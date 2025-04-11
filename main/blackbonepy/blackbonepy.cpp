7d969a05713f7bade75c6340db79b758ce7ac765
ratsafalig <ratsafalig@outlook.com>
2025-04-12 00:24:22 +0800

    upd: add get export

--- main/blackbonepy/blackbonepy.cpp
+++ main/blackbonepy/blackbonepy.cpp
@@ -28,7 +28,10 @@ PYBIND11_MODULE(blackbonepy, m)
 
     py::class_<blackbone::ModuleDataPtr>(m, "ModuleDataPtr");
 
-    py::class_<blackbone::ModuleData, std::shared_ptr<blackbone::ModuleData>>(m, "ModuleData");
+    py::class_<blackbone::ModuleData, std::shared_ptr<blackbone::ModuleData>>(m, "ModuleData")
+        .def_readwrite("name", &blackbone::ModuleData::name)
+        .def_readwrite("fullPath",&blackbone::ModuleData::fullPath)
+        .def_readwrite("baseAddress", &blackbone::ModuleData::baseAddress);
 
     py::class_<blackbone::ProcessModules::mapModules>(m, "mapModules");
 
@@ -38,9 +41,20 @@ PYBIND11_MODULE(blackbonepy, m)
         .def("Detach", &blackbone::Process::Detach)
         .def("Resume", &blackbone::Process::Resume)
         .def("EnumByName", &blackbone::Process::EnumByName)
+        .def("CreateAndAttach", &blackbone::Process::CreateAndAttach)
         .def("modules", &blackbone::Process::modules, py::return_value_policy::reference);
 
     py::class_<blackbone::ProcessModules>(m, "ProcessModules")
         .def("GetAllModules", &blackbone::ProcessModules::GetAllModules)
-        .def("GetMainModule", &blackbone::ProcessModules::GetMainModule);
+        .def("GetMainModule", &blackbone::ProcessModules::GetMainModule)
+        .def("GetExport", [](blackbone::ProcessModules & self, wchar_t * dll, char * name) {
+            return self.GetExport(dll, name).result();
+        });
+
+    py::class_<blackbone::exportData>(m, "exportData")
+        .def_readwrite("forwardName", &blackbone::exportData::forwardName)
+        .def_readwrite("forwardModule", &blackbone::exportData::forwardModule)
+        .def_readwrite("procAddress", &blackbone::exportData::procAddress)
+        .def_readwrite("forwardOrdinal", &blackbone::exportData::forwardOrdinal)
+        .def_readwrite("forwardByOrd", &blackbone::exportData::forwardByOrd);
 }
--- python/main.py
+++ python/main.py
@@ -1,15 +1,35 @@
 import blackbonepy
 import const
+import pefile
 
-r = blackbonepy.Process.EnumByName("msedge.exe")
+# r = blackbonepy.Process.EnumByName("powershell.exe")
 
-for i in r:
-    p = blackbonepy.Process()
-    result = p.Attach(i, const.DEFAULT_ACCESS_P)
+r = blackbonepy.Process.EnumByName("LuckyHunter.exe")
 
-    modules = p.modules()
+dll_path = "d:/steam/steamapps/common/luckyhunter/luckyhunterwindows/gameassembly.dll"
 
-    modules.GetAllModules()
+def get_all_exports(dll_path):
+    pe = pefile.PE(dll_path)
+    if hasattr(pe, 'DIRECTORY_ENTRY_EXPORT'):
+        exports = pe.DIRECTORY_ENTRY_EXPORT.symbols
+        for export in exports:
+            name = export.name.decode('utf-8')
+            export.address
+            export.ordinal
+            print(name)
 
-    pass
+# get_all_exports(dll_path)
 
+def get_export():
+    for i in r:
+        p = blackbonepy.Process()
+
+        result = p.Attach(i, const.DEFAULT_ACCESS_P)
+
+        modules = p.modules()
+
+        rr = modules.GetExport("GameAssembly.dll", "mono_type_size")
+
+        rr = modules.GetAllModules(blackbonepy.eModSeachType.LdrList)
+
+get_export()
